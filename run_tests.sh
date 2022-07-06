6692af56fea763b7be2c86fb5b5d27ff498729f2
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2022-07-06 19:53:45 +0530

    [Add]: show verbose output in tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,9 @@ echo "...running unit tests..."
 export CASKDB_ENV=Test
 mkdir logs/
 mkdir logs/test/
-go test -race -run="^(Test|Benchmark)[^_](.*)" ./... > logs/test/unit.log
+# go test -race -run="^(Test|Benchmark)[^_](.*)" ./... > logs/test/unit.log
+go test -race -run="^(Test|Benchmark)[^_](.*)" ./...
+
 code=$?
 
 if [ $code -ne 0 ]; then
@@ -14,7 +16,7 @@ printf "\nUnit Tests complete. Performing Integration Tests now.\n"
 echo "...running integration tests..."
 
 # go test -v -race -run="^(Test|Benchmark)_(.*)" ./... > logs/test/integration.log
-go test -v -race -run="^(Test|Benchmark)_(.*)" ./... > logs/test/integration.log
+go test -v -race -run="^(Test|Benchmark)_(.*)" ./...
 
 code=$?
 echo $code
